e8c8d20445ed0ca6a05954d65ef668df59ac0c08
Sébastien Crème <sebastien.creme@gmail.com>
2010-11-26 14:28:36 +0100

    4 spaces

--- app/jobs/FetchEmails.java
+++ app/jobs/FetchEmails.java
@@ -19,88 +19,87 @@ import play.libs.IO;
 
 public class FetchEmails extends Job {
 
-  public void doJob() throws Exception {
+    public void doJob() throws Exception {
 
-    // Connect to gmail mailbox
-    Properties props = new Properties();
-    props.setProperty("mail.imap.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
-    props.setProperty("mail.imap.socketFactory.port", "993");
-    Session session = Session.getDefaultInstance(props);
-    Store store = session.getStore("imap");
-    store.connect("imap.gmail.com", Play.configuration.getProperty("mailbox.username"), Play.configuration
-        .getProperty("mailbox.password"));
+        // Connect to gmail mailbox
+        Properties props = new Properties();
+        props.setProperty("mail.imap.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+        props.setProperty("mail.imap.socketFactory.port", "993");
+        Session session = Session.getDefaultInstance(props);
+        Store store = session.getStore("imap");
+        store.connect("imap.gmail.com", Play.configuration.getProperty("mailbox.username"), Play.configuration
+                .getProperty("mailbox.password"));
 
-    // Open jobs mailbox
-    Folder folder = store.getFolder("jobs");
-    folder.open(Folder.READ_WRITE);
+        // Open jobs mailbox
+        Folder folder = store.getFolder("jobs");
+        folder.open(Folder.READ_WRITE);
 
-    // Search unstarred messages
-    SearchTerm unstarred = new FlagTerm(new Flags(Flags.Flag.FLAGGED), false);
-    Message[] messages = folder.search(unstarred);
-    
-    for (Message message : messages) {
-      
-      
-      String contentString = "";
-      
-      List<Attachment> attachments = new ArrayList<Attachment>();
-      if (message.getContent() instanceof String) {
-        contentString = (String) message.getContent();
-      } else if (message.getContent() instanceof Multipart) {
-        Multipart mp = (Multipart) message.getContent();
-        for (int j = 0; j < mp.getCount(); j++) {
-          Part part = mp.getBodyPart(j);
+        // Search unstarred messages
+        SearchTerm unstarred = new FlagTerm(new Flags(Flags.Flag.FLAGGED), false);
+        Message[] messages = folder.search(unstarred);
 
-          String disposition = part.getDisposition();
+        for (Message message : messages) {
 
-          if (disposition == null || 
-              ((disposition != null) && (disposition.equalsIgnoreCase(Part.ATTACHMENT) || disposition.equalsIgnoreCase(Part.INLINE)))
-            ) {
-            // Check if plain
-            MimeBodyPart mbp = (MimeBodyPart) part;
-            if (mbp.isMimeType("text/plain")) {
-              contentString += (String) mbp.getContent();
-            } else {
-              attachments.add(saveAttachment(part));
+            String contentString = "";
+
+            List<Attachment> attachments = new ArrayList<Attachment>();
+            if (message.getContent() instanceof String) {
+                contentString = (String) message.getContent();
+            } else if (message.getContent() instanceof Multipart) {
+                Multipart mp = (Multipart) message.getContent();
+                for (int j = 0; j < mp.getCount(); j++) {
+                    Part part = mp.getBodyPart(j);
+
+                    String disposition = part.getDisposition();
+
+                    if (disposition == null
+                            || ((disposition != null) && (disposition.equalsIgnoreCase(Part.ATTACHMENT) || disposition
+                                    .equalsIgnoreCase(Part.INLINE)))) {
+                        // Check if plain
+                        MimeBodyPart mbp = (MimeBodyPart) part;
+                        if (mbp.isMimeType("text/plain")) {
+                            contentString += (String) mbp.getContent();
+                        } else {
+                            attachments.add(saveAttachment(part));
+                        }
+                    }
+                }
             }
+            Logger.info("content : %s -- attachements (%d)", contentString, attachments.size());
+            String name = ((InternetAddress) message.getFrom()[0]).getPersonal();
+            String email = ((InternetAddress) message.getFrom()[0]).getAddress();
+            JobApplication application = new JobApplication(name, email, contentString, attachments);
+            application.save();
+            if (Play.mode == Play.Mode.PROD) {
+                message.setFlag(Flag.FLAGGED, true);
             }
-          }
-      }
-      Logger.info("content : %s -- attachements (%d)", contentString, attachments.size());
-      String name = ((InternetAddress) message.getFrom()[0]).getPersonal();
-      String email =  ((InternetAddress) message.getFrom()[0]).getAddress();
-      JobApplication application = new JobApplication(name, email, contentString, attachments);
-      application.save();
-      if (Play.mode == Play.Mode.PROD) {
-        message.setFlag(Flag.FLAGGED, true);
-      }
+        }
+
+        // Close connection
+        folder.close(false);
+        store.close();
     }
-
-    // Close connection
-    folder.close(false);
-    store.close();
-  }
-  private Attachment saveAttachment(Part part) throws Exception, MessagingException, IOException {
-    // Special non-attachment cases here of
-    // image/gif, text/html, ...
-    Attachment attachment = new Attachment();
-    attachment.name = decodeName(part.getFileName());
-    attachment.content.set(part.getInputStream(), part.getContentType());
-    attachment.save();
-    return attachment;
-  }
-  protected String decodeName(String name) throws Exception {
-    if (name == null || name.length() == 0) {
-      return "unknown";
+    private Attachment saveAttachment(Part part) throws Exception, MessagingException, IOException {
+        // Special non-attachment cases here of
+        // image/gif, text/html, ...
+        Attachment attachment = new Attachment();
+        attachment.name = decodeName(part.getFileName());
+        attachment.content.set(part.getInputStream(), part.getContentType());
+        attachment.save();
+        return attachment;
     }
-    String ret = java.net.URLDecoder.decode(name, "UTF-8");
+    protected String decodeName(String name) throws Exception {
+        if (name == null || name.length() == 0) {
+            return "unknown";
+        }
+        String ret = java.net.URLDecoder.decode(name, "UTF-8");
 
-    // also check for a few other things in the string:
-    ret = ret.replaceAll("=\\?utf-8\\?q\\?", "");
-    ret = ret.replaceAll("\\?=", "");
-    ret = ret.replaceAll("=20", " ");
+        // also check for a few other things in the string:
+        ret = ret.replaceAll("=\\?utf-8\\?q\\?", "");
+        ret = ret.replaceAll("\\?=", "");
+        ret = ret.replaceAll("=20", " ");
 
-    return ret;
-  }
+        return ret;
+    }
 
 }
