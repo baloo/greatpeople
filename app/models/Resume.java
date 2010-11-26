a4726b024264afc8b92e55b2654f78380246acad
Erwan Loisant <eloisant@gmail.com>
2010-11-26 11:33:30 +0100

    crud works

--- app/models/Resume.java
+++ app/models/Resume.java
@@ -1,10 +1,14 @@
 package models;
 
+import java.util.Arrays;
+import java.util.List;
+
 import javax.persistence.Entity;
 
 import play.data.validation.Email;
 import play.db.jpa.Blob;
 import play.db.jpa.Model;
+import play.utils.Utils;
 
 @Entity
 public class Resume extends Model {
@@ -16,7 +20,17 @@ public class Resume extends Model {
 
     public String phone;
 
+    // Separated by commas
+    public String tags;
+
     public Blob attachment;
 
+    public List<String> tagsList() {
+        return Arrays.asList(tags.split(","));
+    }
+
+    public void changeTags(List<String> tagsList) {
+        tags = Utils.join(tagsList, ",");
+    }
 
 }
