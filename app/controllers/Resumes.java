d293c5712cbb81be2493a12c372a99015b530070
Erwan Loisant <eloisant@gmail.com>
2010-11-26 13:23:09 +0100

    Move attachments to a separate part

--- app/controllers/Resumes.java
+++
@@ -1,7 +0,0 @@
-package controllers;
-
-import play.mvc.*;
-
-@With(Secure.class)
-public class Resumes extends CRUD {
-}
--- app/models/Resume.java
+++ app/models/Resume.java
@@ -4,14 +4,18 @@ import java.util.Arrays;
 import java.util.List;
 
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 
 import play.data.validation.Email;
-import play.db.jpa.Blob;
 import play.db.jpa.Model;
 import play.utils.Utils;
 
 @Entity
 public class Resume extends Model {
+
+    public static final int STATUS_NEW = 0;
+    public static final int STATUS_INPROGRESS = 2;
+    public static final int STATUS_ARCHIVED = 3;
 
     public String name;
 
@@ -23,7 +27,10 @@ public class Resume extends Model {
     // Separated by commas
     public String tags;
 
-    public Blob attachment;
+    public int status;
+
+    @OneToMany
+    public List<Attachment> attachments;
 
     public List<String> tagsList() {
         return Arrays.asList(tags.split(","));
@@ -31,6 +38,11 @@ public class Resume extends Model {
 
     public void changeTags(List<String> tagsList) {
         tags = Utils.join(tagsList, ",");
+    }
+
+    @Override
+    public String toString() {
+        return name + " <" + email + ">";
     }
 
 }
