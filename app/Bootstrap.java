77dec3e99aec7a27db0c27afc34b31550d4f4198
Erwan Loisant <eloisant@gmail.com>
2010-11-26 13:24:57 +0100

    Fix bootstrap

--- app/Bootstrap.java
+++ app/Bootstrap.java
@@ -1,4 +1,4 @@
-import models.User;
+import models.Resume;
 import play.Play;
 import play.jobs.Job;
 import play.jobs.OnApplicationStart;
@@ -9,7 +9,7 @@ public class Bootstrap extends Job {
 
     public void doJob() {
         if(Play.mode == Play.Mode.DEV) {
-            if (User.count() == 0) {
+            if (Resume.count() == 0) {
                 Fixtures.load("bootstrap/initialdata.yml");
             }
         }
